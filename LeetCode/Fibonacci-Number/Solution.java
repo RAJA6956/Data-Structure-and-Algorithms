f67325a489217722476fc9e5e2fb51646c000461
RAJA BABU KAUSHAL <155283671+RAJA6956@users.noreply.github.com>
2025-12-04 10:17:25 +0530

    Add solution for Fibonacci Number

--- LeetCode/Fibonacci-Number/Solution.java
+++ LeetCode/Fibonacci-Number/Solution.java
@@ -1,5 +1,18 @@
-int[] ans = 
-{0,1,1,2,3,5,8,13,21,34,55,89,144,
-233,377,610,987,1597,2584,4181,
-6765,10946,17711,28657,46368,
-75025,121393,196418,317811,514229,832040};
+1// bottom up-dp approach with space optimization without usingg dp array
+2class Solution {
+3    public int fib(int n) {
+4        if(n<=1) return n;
+5        int ans=0;
+6        int first=0;
+7        int second=1;
+8        
+9        for(int i=2;i<=n;i++){
+10            ans=first+second;
+11            first=second;
+12            second=ans;
+13        }
+14        return ans;
+15        
+16    }
+17    
+18}
