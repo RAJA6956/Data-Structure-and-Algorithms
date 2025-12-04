064d5198eb646b96b3abf73b35d5b42496a53f00
RAJA BABU KAUSHAL <155283671+RAJA6956@users.noreply.github.com>
2025-12-04 15:50:15 +0530

    Add solution for Monthly Transactions I

--- LeetCode/Monthly-Transactions-I/Solution.sql
+++ LeetCode/Monthly-Transactions-I/Solution.sql
@@ -1,6 +1,11 @@
 1# Write your MySQL query statement below
-2SELECT class
-3FROM Courses
-4GROUP BY class
-5HAVING COUNT(DISTINCT student) >= 5;
-6
+2SELECT 
+3    DATE_FORMAT(trans_date, '%Y-%m') AS month,
+4    country,
+5    COUNT(*) AS trans_count,
+6    SUM(IF(state = 'approved', 1, 0)) AS approved_count,
+7    SUM(amount) AS trans_total_amount,
+8    SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
+9FROM Transactions
+10GROUP BY month, country;
+11
