bacc08cff899550d09eed04f41f78d2e86a0fdd8
RAJA BABU KAUSHAL <155283671+RAJA6956@users.noreply.github.com>
2025-12-11 22:07:48 +0530

    Add solution for Climbing Stairs

--- LeetCode/Climbing-Stairs/Solution.java
+++ LeetCode/Climbing-Stairs/Solution.java
@@ -34,30 +34,47 @@
 34
 35
 36/// write top down - memorization approach
-37class Solution {
+37// class Solution {
 38    
 39
-40    public int climbStairs(int n) {
-41        int[] dp=new int[n+1];
-42        Arrays.fill(dp,-1);
-43        return solve(dp,n);
-44    }
-45    public int solve(int[] dp,int n){
-46        if(n<=1) return 1;
-47        if(dp[n] != -1) return dp[n];
-48
-49        return dp[n] = solve(dp,n-2) + solve(dp,n-1);
-50    }
-51}
-52
-53// Recursive approach
-54// class Solution {
-55
-56//     public int climbStairs(int n) {
-57//         if(n<=1) return 1;
-58//         int two=climbStairs(n-2);
-59//         int one=climbStairs(n-1);
-60//         return one+two;
-61//     }
-62// }
-63
+40//     public int climbStairs(int n) {
+41//         int[] dp=new int[n+1];
+42//         Arrays.fill(dp,-1);
+43//         return solve(dp,n);
+44//     }
+45//     public int solve(int[] dp,int n){
+46//         if(n<=1) return 1;
+47//         if(dp[n] != -1) return dp[n];
+48        
+49
+50//         return dp[n] = solve(dp,n-2) + solve(dp,n-1);
+51//     }
+52// }
+53
+54// Recursive approach
+55// class Solution {
+56
+57//     public int climbStairs(int n) {
+58//         if(n<=1) return 1;
+59//         int two=climbStairs(n-2);
+60//         int one=climbStairs(n-1);
+61//         return one+two;
+62//     }
+63// }
+64
+65// Tabulation
+66class Solution {
+67    
+68
+69    public int climbStairs(int n) {
+70        int[] dp=new int[n+1];
+71        dp[0]=1;
+72        dp[1]=1;
+73        for(int i =2; i<=n;i++){
+74            dp[i] = dp[i-1]+dp[i-2];
+75        }
+76        return dp[n];
+77    
+78    }
+79}
+80
