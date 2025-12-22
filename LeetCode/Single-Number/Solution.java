b6e5badcb85ba31a4c0d0ca0ccd1b2455d4daee5
RAJA BABU KAUSHAL <155283671+RAJA6956@users.noreply.github.com>
2025-12-22 14:46:01 +0530

    Add solution for Single Number

---
+++ LeetCode/Single-Number/Solution.java
@@ -0,0 +1,11 @@
+1class Solution {
+2    public int singleNumber(int[] nums) {
+3        int result = 0;
+4        for (int num : nums) {
+5            result ^= num; // XOR all numbers
+6        }
+7        return result;
+8    }
+9}
+10
+11
