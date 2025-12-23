bcd4d9789417957e674c8a15f4be9b3f2e998070
RAJA BABU KAUSHAL <155283671+RAJA6956@users.noreply.github.com>
2025-12-23 17:35:19 +0530

    Add solution for Smallest Integer Divisible by K

--- LeetCode/Smallest-Integer-Divisible-by-K/Solution.java
+++ LeetCode/Smallest-Integer-Divisible-by-K/Solution.java
@@ -1,18 +1,18 @@
-class Solution {
-  public int smallestRepunitDivByK(int k) {
-    if (k % 10 != 1 && k % 10 != 3 && k % 10 != 7 && k % 10 != 9)
-      return -1;
-    Set<Integer> seen = new HashSet<>();
-    int n = 0;
-    for (int length = 1; length <= k; ++length) {
-      n = (n * 10 + 1) % k;
-      if (n == 0)
-        return length;
-      if (seen.contains(n))
-        return -1;
-      seen.add(n);
-    }
-
-    return -1;
-  }
-}
+1class Solution {
+2  public int smallestRepunitDivByK(int k) {
+3    if (k % 10 != 1 && k % 10 != 3 && k % 10 != 7 && k % 10 != 9)
+4      return -1;
+5    Set<Integer> seen = new HashSet<>();
+6    int n = 0;
+7    for (int length = 1; length <= k; ++length) {
+8      n = (n * 10 + 1) % k;
+9      if (n == 0)
+10        return length;
+11      if (seen.contains(n))
+12        return -1;
+13      seen.add(n);
+14    }
+15
+16    return -1;
+17  }
+18}
