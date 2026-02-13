08b684cdb3b68ab3d5da0ceb56838b9d26e48ac6
RAJA BABU KAUSHAL <155283671+RAJA6956@users.noreply.github.com>
2026-02-13 17:40:08 +0530

    Add solution for Find the Town Judge

--- LeetCode/Find-the-Town-Judge/Solution.java
+++ LeetCode/Find-the-Town-Judge/Solution.java
@@ -1,19 +1,14 @@
 1class Solution {
 2    public int findJudge(int n, int[][] trust) {
-3        int[] indegree = new int[n + 1];
-4        int[] outdegree=new int[n+1];
-5        for (int[] e : trust) {
-6            indegree[e[1]]++;
-7            outdegree[e[0]]++;
+3        int[] inDegree=new int[n+1];
+4        int [] outDegree=new int[n+1];
+5        for(int edge[] :trust){
+6            inDegree[edge[1]]++;
+7            outDegree[edge[0]]++;
 8        }
-9
-10        for (int i = 1; i <= n; i++) {
-11            if (indegree[i] == n - 1  && outdegree[i] == 0) return i;
-12        }
-13        return -1;
-14        
-15        
-16    }
-17}
-18
-19
+9        for(int i=1;i<=n;i++){
+10            if(outDegree[i]==0 && inDegree[i]==n-1) return i;
+11        }
+12        return -1;     
+13    }
+14}
