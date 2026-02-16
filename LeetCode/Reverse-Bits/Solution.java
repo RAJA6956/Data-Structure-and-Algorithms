2991ed3b50a40afe3b184c6248cd296f27ebe127
RAJA BABU KAUSHAL <155283671+RAJA6956@users.noreply.github.com>
2026-02-16 18:16:56 +0530

    Add solution for Reverse Bits

--- LeetCode/Reverse-Bits/Solution.java
+++ LeetCode/Reverse-Bits/Solution.java
@@ -3,10 +3,9 @@
 3    public int reverseBits(int n) {
 4        int result = 0;
 5        for (int i = 0; i < 32; i++) {
-6            int bit = (n >> i) & 1;   // extract the i-th bit
-7            result |= (bit << (31 - i)); // place it in the reversed position
+6            int bit = (n >> i) & 1;   
+7            result |= (bit << (31 - i)); 
 8        }
 9        return result;
 10    }
 11}
-12
