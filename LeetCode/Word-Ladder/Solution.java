89be013e2a4427c8c15b1c3f1531358c4096e94d
RAJA BABU KAUSHAL <155283671+RAJA6956@users.noreply.github.com>
2026-02-17 02:09:11 +0530

    Add solution for Word Ladder

--- LeetCode/Word-Ladder/Solution.java
+++ LeetCode/Word-Ladder/Solution.java
@@ -1,45 +1,38 @@
-import java.util.*;
-
-class Solution {
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> set = new HashSet<>(wordList);
-        if (!set.contains(endWord)) {
-            return 0;
-        }
-
-        Queue<String> queue = new LinkedList<>();
-        queue.add(beginWord);
-
-        int level = 1; 
-        while (!queue.isEmpty()) {
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                String word = queue.poll();
-                char[] chars = word.toCharArray();
-
-                for (int j = 0; j < chars.length; j++) {
-                    char original = chars[j];
-
-                    for (char c = 'a'; c <= 'z'; c++) {
-                        chars[j] = c;
-                        String newWord = new String(chars);
-
-                        if (newWord.equals(endWord)) {
-                            return level + 1;
-                        }
-
-                        if (set.contains(newWord)) {
-                            queue.add(newWord);
-                            set.remove(newWord); 
-                        }
-                    }
-
-                    chars[j] = original; 
-                }
-            }
-            level++;
-        }
-
-        return 0;
-    }
-}
+1class Solution {
+2    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+3        Set<String> set = new HashSet<>(wordList);
+4        if (!set.contains(endWord)) {
+5            return 0;
+6        }
+7        Queue<String> q=new LinkedList<>();
+8        q.add(beginWord);
+9        int level=1; 
+10        while (!q.isEmpty()) {
+11            int size=q.size();
+12            for (int i=0;i<size;i++) {
+13                String word = q.poll();
+14                char[] chars = word.toCharArray();
+15
+16                for(int j=0;j< chars.length;j++){
+17                    char original = chars[j];
+18
+19                    for (char c = 'a'; c <= 'z'; c++) {
+20                        chars[j] = c;
+21                        String newWord = new String(chars);
+22
+23                        if (newWord.equals(endWord)) {
+24                            return level+1;
+25                        }
+26                        if (set.contains(newWord)) {
+27                            q.add(newWord);
+28                            set.remove(newWord); 
+29                        }
+30                    }
+31                    chars[j]=original;
+32                }
+33            }
+34            level++;   
+35        }
+36        return 0;
+37    }
+38}
