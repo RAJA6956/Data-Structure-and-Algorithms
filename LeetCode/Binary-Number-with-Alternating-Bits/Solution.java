9253a4f9057405d9b1e9ffa683fa2feba7718519
RAJA BABU KAUSHAL <155283671+RAJA6956@users.noreply.github.com>
2026-02-19 02:34:35 +0530

    Add solution for Binary Number with Alternating Bits

--- LeetCode/Binary-Number-with-Alternating-Bits/Solution.java
+++ LeetCode/Binary-Number-with-Alternating-Bits/Solution.java
@@ -1,11 +1,6 @@
-function bitSolution(number){
-    //      10101010101
-    //  +    1010101010    ( number >> 1 )
-    //  ---------------
-    //  =   11111111111
-    //  &  100000000000
-    //  ---------------
-    //  =             0    ( power of two )
-    let tmp = ( number >> 1 ) + number;
-    return (tmp & tmp + 1) === 0;
-}
+1class Solution {
+2    public boolean hasAlternatingBits(int n) {
+3        int x = n ^ (n >> 1);
+4        return (x & (x + 1)) == 0;
+5    }
+6}
