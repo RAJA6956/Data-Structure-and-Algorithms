2a86d7b7d2e0cbfe9a4787aac7eef65e0d53a132
RAJA BABU KAUSHAL <155283671+RAJA6956@users.noreply.github.com>
2026-02-21 21:14:15 +0530

    Add solution for Find if Path Exists in Graph

--- LeetCode/Find-if-Path-Exists-in-Graph/Solution.java
+++ LeetCode/Find-if-Path-Exists-in-Graph/Solution.java
@@ -1,52 +1,36 @@
-/**
-
-Example 1:
-
-isConnected = [[1,1,0],[1,1,0],[0,0,1]]
-
- CITY |0 1 2
-------|------
-     0|1 1 0 
-     1|1 1 0
-     2|0 0 1
-
-
-Connections:
-0 ► 1
-1 ► 0
-2 ► none
-
-
-Example 2:
-
-isConnected = [[1,0,0],[0,1,0],[0,0,1]]
-
- |0 1 2
--|------
-0|1 0 0 
-1|0 1 0
-2|0 0 1
-
-
-Connections:
-0 ► none
-1 ► none
-2 ► none
-
-
-Example 3: 
-
- |0 1 2 3
----------
-0|1 1 0 0 
-1|1 1 1 0
-2|0 1 1 1
-3|0 0 1 1
-
-
-0 ► 1
-1 ► 0 2
-2 ► 1 3
-3 ► 2
-
-**/
+1class Solution {
+2    public boolean validPath(int n, int[][] edges, int source, int destination) {
+3        ArrayList<ArrayList<Integer>> list=new ArrayList<>();
+4        for(int i=0;i<n;i++){
+5            list.add(new ArrayList<>());
+6        }
+7        for(int[] edge: edges){
+8            list.get(edge[0]).add(edge[1]);
+9            list.get(edge[1]).add(edge[0]);
+10
+11        }
+12        Queue<Integer> q=new LinkedList<>();
+13        boolean[] visited=new boolean[n];
+14        q.add(source);
+15        visited[source]=true;
+16        while(! q.isEmpty()){
+17            int node=q.poll();
+18            if(node==destination) return true;
+19            for(int neighbour: list.get(node)){
+20                if(!visited[neighbour]){
+21                    visited[neighbour]=true;
+22                    q.add(neighbour);
+23
+24                }
+25            }
+26        }
+27        return false;
+28
+29        
+30
+31
+32        
+33
+34        
+35    }
+36}
