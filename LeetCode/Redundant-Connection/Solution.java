b4657b38882999a8cb4e2af2c6d35e8ad6d5c235
RAJA BABU KAUSHAL <155283671+RAJA6956@users.noreply.github.com>
2026-03-02 17:12:38 +0530

    Add solution for Redundant Connection

--- LeetCode/Redundant-Connection/Solution.java
+++ LeetCode/Redundant-Connection/Solution.java
@@ -1,49 +1,26 @@
-1class Solution {
-2
-3    public int[] findRedundantConnection(int[][] edges) {
-4
-5        int n = edges.length;
-6        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-7
-8        for (int i = 0; i <= n; i++) {
-9            graph.add(new ArrayList<>());
-10        }
-11
-12        for (int[] edge : edges) {
-13
-14            int u = edge[0];
-15            int v = edge[1];
-16
-17            boolean[] visited = new boolean[n + 1];
-18
-19            if (!graph.get(u).isEmpty() &&
-20                !graph.get(v).isEmpty() &&
-21                dfs(u, v, graph, visited)) {
-22                return edge;
-23            }
-24
-25            graph.get(u).add(v);
-26            graph.get(v).add(u);
-27        }
-28
-29        return new int[0];
-30    }
-31
-32    private boolean dfs(int src, int target,
-33                        ArrayList<ArrayList<Integer>> graph,
-34                        boolean[] visited) {
-35
-36        if (src == target) return true;
-37
-38        visited[src] = true;
-39
-40        for (int neighbor : graph.get(src)) {
-41            if (!visited[neighbor]) {
-42                if (dfs(neighbor, target, graph, visited))
-43                    return true;
-44            }
-45        }
-46
-47        return false;
-48    }
-49}
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        adj = defaultdict(list)
+        indegree = [0] * (len(edges) + 1)
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+            indegree[v] += 1
+            indegree[u] += 1
+
+        q = deque()
+        for node in range(1, len(edges) + 1):
+            if indegree[node] == 1:
+                q.append(node)
+        
+        while q:
+            node = q.popleft()
+            for nextNode in adj[node]:
+                indegree[nextNode] -= 1
+                indegree[node] -= 1
+                if indegree[nextNode] == 1:
+                    q.append(nextNode)
+        
+        for u, v in reversed(edges):
+            if indegree[u] > 0 and indegree[v] > 0:
+                return [u, v]
