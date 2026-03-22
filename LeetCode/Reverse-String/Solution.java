d0b45167b594a90601c9f063606fae16582ac3ba
RAJA BABU KAUSHAL <155283671+RAJA6956@users.noreply.github.com>
2026-03-22 20:13:13 +0530

    Add solution for Reverse String

--- LeetCode/Reverse-String/Solution.java
+++ LeetCode/Reverse-String/Solution.java
@@ -1,17 +1,17 @@
 class Solution {
-    public void reverseString(char[] s) {
-        int l=0;
-        int r=s.length-1;
-        reverse(s,l,r);
-        
+    //By recursion
+    public static void reverseString(char[] s) {
+         swap(s,0,s.length-1);
     }
-    public static char[] reverse(char[] s, int l, int r){
-        if(l>=r){
-            return s;
-        }
-        char temp=s[l];
-        s[l]=s[r];
-        s[r]=temp;
-        return reverse(s,l+1,r-1);
+    public static void swap(char[] s, int i, int j) {
+        if (i >= j) return;
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+        swap(s, i + 1, j - 1);
+    }
+    public static void main(String[] args){
+        char[] s = {'h','e','l','l','o'};
+        reverseString(s);
     }
 }
