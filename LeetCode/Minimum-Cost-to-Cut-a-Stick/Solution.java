c669aae7d87bf7bb3d09f1bd9dfc6f4263165074
RAJA BABU KAUSHAL <155283671+RAJA6956@users.noreply.github.com>
2026-03-27 10:13:49 +0530

    Add solution for Minimum Cost to Cut a Stick

--- LeetCode/Minimum-Cost-to-Cut-a-Stick/Solution.java
+++ LeetCode/Minimum-Cost-to-Cut-a-Stick/Solution.java
@@ -1,28 +1,28 @@
-class Solution {
-    int[][] dp;
-    int[] cuts;
-
-    public int minCost(int n, int[] cuts) {
-        Arrays.sort(cuts);
-        this.cuts = cuts;
-        dp = new int[cuts.length][cuts.length];
-        return cost(0, cuts.length - 1, 0, n);
-    }
-
-    private int cost(int i, int j, int start, int end) {
-        if (i > j) {
-            return 0; // no cuts to perform
-        }
-        if (dp[i][j] != 0) {
-            return dp[i][j]; // already computed
-        }
-        int minCost = Integer.MAX_VALUE;
-        for (int k = i; k <= j; k++) {
-            int fs = cost(i, k - 1, start, cuts[k]); // left stick
-            int ss = cost(k + 1, j, cuts[k], end);   // right stick
-            int self = end - start;                  // cost of current cut
-            minCost = Math.min(minCost, fs + ss + self);
-        }
-        return dp[i][j] = minCost;
-    }
-}
+1class Solution {
+2    int[][] dp;
+3    int[] cuts;
+4
+5    public int minCost(int n, int[] cuts) {
+6        Arrays.sort(cuts);
+7        this.cuts = cuts;
+8        dp = new int[cuts.length][cuts.length];
+9        return cost(0, cuts.length - 1, 0, n);
+10    }
+11
+12    private int cost(int i, int j, int start, int end) {
+13        if (i > j) {
+14            return 0; 
+15        }
+16        if (dp[i][j] != 0) {
+17            return dp[i][j]; 
+18        }
+19        int minCost = Integer.MAX_VALUE;
+20        for (int k = i; k <= j; k++) {
+21            int fs = cost(i, k - 1, start, cuts[k]); 
+22            int ss = cost(k + 1, j, cuts[k], end);   
+23            int self = end - start;                  
+24            minCost = Math.min(minCost, fs + ss + self);
+25        }
+26        return dp[i][j] = minCost;
+27    }
+28}
