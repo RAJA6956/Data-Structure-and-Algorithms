39ad1952ad1f93f1a21ee282d9e074b6e873f81c
RAJA BABU KAUSHAL <155283671+RAJA6956@users.noreply.github.com>
2026-04-24 12:14:13 +0530

    Add solution for Find Center of Star Graph

--- LeetCode/Find-Center-of-Star-Graph/Solution.java
+++ LeetCode/Find-Center-of-Star-Graph/Solution.java
@@ -1,16 +1,4 @@
-1class Solution {
-2    public int findCenter(int[][] edges) {
-3        int e=edges.length;
-4        int n=e+1;
-5        int[] degree=new int[n+1];
-6        for(int[] edge: edges){
-7            degree[edge[0]]++;
-8            degree[edge[1]]++;
-9            if((degree[edge[0]])>1) return edge[0];
-10            else if((degree[edge[1]])>1) return edge[1];
-11        }
-12        return 0;
-13
-14        
-15    }
-16}
+class Solution:
+    def findCenter(self, edges):
+        
+        return edges[0][0] if edges[0][0] in (edges[1][0], edges[1][1]) else edges[0][1]
